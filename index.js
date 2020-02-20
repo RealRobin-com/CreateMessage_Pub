6daedc37e519978bdf9c407d70ea9ffa6d63f8c2
Justin Robinson <jurobn@amazon.com>
2020-02-20 00:03:36 -0500

    publish events to create message queuee

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const AWS = require('aws-sdk');
+AWS.config.update({ region: process.env.AWS_REGION });
+
 exports.handler = async (event, context, callback) => {
-  // TODO implement
-  const response = {
-      statusCode: 200,
-      body: "justin from lambda",
+
+  const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+
+  const params = {
+    MessageBody: JSON.stringify(event),
+    QueueUrl: "https://sqs.us-east-1.amazonaws.com/806302365898/CreateMessage"
   };
-  callback(null, response);
+
+  sqs.sendMessage(params, function (err, data) {
+    if(err) {
+      console.log('error:',"Fail Send Message" + err);
+      callback("ERROR Put SQS");  // ERROR with message
+    }else{
+      console.log('data:',data.MessageId);
+      callback(null, '');  // SUCCESS 
+    }
+  });
 };
